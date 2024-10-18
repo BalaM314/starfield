999bedef35718a4e519943a5599d09be9dd6d895
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2024-10-18 12:52:30 +0530

    scrolling with decent performance

--- src/ui.js
+++ src/ui.js
@@ -2,20 +2,38 @@ import { Starfield } from "./starfield.js";
 import { getElement, Rand } from "./utils.js";
 const canvas = getElement("canvas", HTMLCanvasElement);
 const field = new Starfield(canvas);
+const shouldScroll = new URLSearchParams(location.search).has("scroll");
+let frame = 0;
+let needsRedraw = false;
 function loop() {
-    if (canvas.width != document.body.clientWidth)
+    frame++;
+    if (canvas.width != document.body.clientWidth) {
         canvas.width = document.body.clientWidth;
-    if (canvas.height != document.body.clientHeight)
+        needsRedraw = true;
+    }
+    if (canvas.height != document.body.clientHeight) {
         canvas.height = document.body.clientHeight;
-    if (field.stars.length == 0)
+        needsRedraw = true;
+    }
+    if (field.stars.length == 0) {
         field.generate();
-    field.draw();
-    // field.scrollWrap(4, 1);
+        needsRedraw = true;
+    }
+    if (frame % 4 == 0 && shouldScroll) {
+        field.scrollWrap(0.3, 0.1);
+        needsRedraw = true;
+    }
+    if (needsRedraw) {
+        field.draw();
+        needsRedraw = false;
+    }
     window.cancel = requestAnimationFrame(loop);
 }
 addEventListener("keydown", e => {
-    if (!e.altKey && !e.shiftKey && !e.ctrlKey && !["tab", "capslock", "ctrl", "shift", "alt"].includes(e.key.toLowerCase()))
+    if (!e.altKey && !e.shiftKey && !e.ctrlKey && !["tab", "capslock", "ctrl", "shift", "alt", "f12"].includes(e.key.toLowerCase())) {
         field.generate();
+        needsRedraw = true;
+    }
 });
 Object.assign(window, { field, Rand });
 loop();
--- src/ui.ts
+++ src/ui.ts
@@ -4,27 +4,48 @@ import { getElement, Rand } from "./utils.js";
 const canvas = getElement("canvas", HTMLCanvasElement);
 
 const field = new Starfield(canvas);
+const shouldScroll = new URLSearchParams(location.search).has("scroll");
 
 declare global {
   var cancel: number;
 }
 
-
+let frame = 0;
+let needsRedraw = false;
 function loop(){
-  if(canvas.width != document.body.clientWidth) canvas.width = document.body.clientWidth;
-  if(canvas.height != document.body.clientHeight) canvas.height = document.body.clientHeight;
+  frame ++;
   
-  if(field.stars.length == 0) field.generate();
-
-  field.draw();
-  // field.scrollWrap(4, 1);
-
+  if(canvas.width != document.body.clientWidth){
+    canvas.width = document.body.clientWidth;
+    needsRedraw = true;
+  }
+  if(canvas.height != document.body.clientHeight){
+    canvas.height = document.body.clientHeight;
+    needsRedraw = true;
+  }
+  
+  if(field.stars.length == 0){
+    field.generate();
+    needsRedraw = true;
+  }
+  if(frame % 4 == 0 && shouldScroll){
+    field.scrollWrap(0.3, 0.1);
+    needsRedraw = true;
+  }
+  if(needsRedraw){
+    field.draw();
+    needsRedraw = false;
+  }
+  
   window.cancel = requestAnimationFrame(loop);
 }
 
+
 addEventListener("keydown", e => {
-  if(!e.altKey && !e.shiftKey && !e.ctrlKey && !["tab", "capslock", "ctrl", "shift", "alt"].includes(e.key.toLowerCase()))
+  if(!e.altKey && !e.shiftKey && !e.ctrlKey && !["tab", "capslock", "ctrl", "shift", "alt", "f12"].includes(e.key.toLowerCase())){
     field.generate();
+    needsRedraw = true;
+  }
 });
 
 Object.assign(window, { field, Rand });
